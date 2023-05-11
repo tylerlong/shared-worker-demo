c325eee3402e8d334f54e5fbdd1463d8e2017bcb
Tyler Long <tyler4long@gmail.com>
2023-05-11 15:54:33 -0700

    Delete useless files

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,12 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './app';
-import store from './store';
 
 const container = document.createElement('div');
 document.body.appendChild(container);
 const root = createRoot(container);
 root.render(
   <StrictMode>
-    <App store={store} />
+    <App />
   </StrictMode>,
 );
