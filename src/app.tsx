cd109246eb8f46d541565aec70a42da73e116921
Tyler Long <tyler4long@gmail.com>
2023-05-11 16:01:31 -0700

    Worker could be TS

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import { Button, Input, Space, Typography, message } from 'antd';
 
 const { Title } = Typography;
 
-const worker = new SharedWorker(new URL('./worker.js', import.meta.url), { type: 'module' });
+const worker = new SharedWorker(new URL('./worker.ts', import.meta.url), { type: 'module' });
 worker.port.onmessage = (e) => {
   message.success(`Received: ${e.data}`);
 };
